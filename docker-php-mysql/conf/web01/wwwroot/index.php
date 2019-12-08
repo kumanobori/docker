8d9342ede5895d84b983d82dab975607094f6ad7
kumanobori <jirosv01@gmail.com>
2019-12-08 15:41:02 +0900

    tzdataのインストールを追加 その他こまごま

--- docker-php-mysql/conf/web01/wwwroot/index.php
+++ docker-php-mysql/conf/web01/wwwroot/index.php
@@ -6,8 +6,11 @@
 	$dbh = new PDO($dsn, $user, $password);
 	$sql = 'select t01.id, t01.name, t02.name as region from m_prefecture t01 inner join m_region t02 on t02.id = t01.region_id order by t01.id';
 	$result = $dbh->query($sql);
+	
+	$timestamp = date("Y-m-d H:i:s");
 ?>
 <html><head></head><body>
+	<p><?php echo $timestamp; ?></p>
 	<table>
 		<tr><th>id</th><th>name</th><th>region</th></tr>
 		<?php while($record = $result->fetch(PDO::FETCH_ASSOC)): ?>
